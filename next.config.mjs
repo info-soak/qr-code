baaec308b300b0950a220107493ca85e4d902405
Info Soak <infosoak1289@gmail.com>
2025-07-24 22:54:06 +0530

    built again

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export", // Enables static HTML export
-    // Optional: Add a base path if your GitHub Pages URL includes a repository name
-    // For a User/Organization site (username.github.io), you typically don't need basePath.
-    // For a Project site (username.github.io/repo-name/), you DO need basePath.
-    // Replace 'your-repo-name' with the actual name of your GitHub repository.
-    basePath: process.env.NODE_ENV === 'production' ? '/qr-code' : '',
 };
 
 export default nextConfig;
